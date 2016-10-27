e380265ac83efaf543bfac3d5ebb903576363e07
kouji happa <kouji-happa@aquaring.co.jp>
2016-10-27 15:45:32 +0900

    差分抽出で「fatal: pathspec」の対策

--- mac_script/gitzip.sh
+++ mac_script/gitzip.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ "$2" = "" ]; then
-    git archive --format=zip --prefix=diff/ HEAD `git diff --name-only HEAD $1` -o /Users/kouji-happa/Desktop/diff.zip
+    git archive --format=zip --prefix=diff/ HEAD `git diff --diff-filter=AMCR --name-only $1 HEAD` -o /Users/kouji-happa/Desktop/diff.zip
 else
-    git archive --format=zip --prefix=diff/ $1 `git diff --name-only $1 $2` -o /Users/kouji-happa/Desktop/diff.zip
+    git archive --format=zip --prefix=diff/ $1 `git diff --diff-filter=AMCR --name-only $2 $1` -o /Users/kouji-happa/Desktop/diff.zip
 fi
